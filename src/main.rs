71242a312153e5e1a49d7cc8486efddc5a42418d
Matthew <staalkoper@gmail.com>
2020-11-03 22:24:05 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+
+// Tests
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("8=======D");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} test", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]")
+}
